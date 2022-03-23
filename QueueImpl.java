edfed49e842cfbda3a0ccecc70a9240298806468
ShifaRayeesa <98878113+ShifaRayeesa@users.noreply.github.com>
2022-03-23 10:44:31 +0530

    Add files via upload

--- QueueImpl.java
+++ QueueImpl.java
@@ -5,16 +5,49 @@ import java.util.Scanner;
 public class QueueImpl {
     Queue<String> reception=new LinkedList<>();
     Queue<String> waitingRoom=new LinkedList<>();
+    String inRoom=null;
+    boolean isIn=false;
+    void display()
+    {
+        System.out.println("In Interview Room -->  "+inRoom);
+        System.out.println("Waiting Room  -->  "+waitingRoom);
+        System.out.println("Reception  -->  "+reception);
+    }
 
+    void callNextCandidate()
+    {
+        if(!isIn && !waitingRoom.isEmpty())
+        {
+            isIn=!isIn;
+            inRoom=waitingRoom.remove();
+            if(!reception.isEmpty())
+                waitingRoom.add(reception.remove());
+            System.out.println(inRoom+" enters interview room");
+        }
+        else System.out.println("Candidate in Room");
+    }
 
-
+    boolean outCandidate()
+    {
+        if(isIn)
+        {
+            System.out.println(inRoom+" leaves interview room");
+            inRoom=null;
+            isIn=!isIn;
+            return true;
+        }
+        else
+        {
+            System.out.println("No one in interview room");
+            return false;
+        }
+    }
     public static void main(String[] args)
     {
-        Scanner sc=new Scanner(System.in);
         QueueImpl queue=new QueueImpl();
-        int candidates,ctr=0,option,waitingRoom;
-        boolean isIn=false;
-        String inRoom=null;
+        Scanner sc=new Scanner(System.in);
+        int candidates,candidatesWaiting,option,ctr=0;
+
         System.out.println("Enter the number of candidates : ");
         candidates=sc.nextInt();
         sc.nextLine();
@@ -25,12 +58,12 @@ public class QueueImpl {
         }
 
         System.out.println("Enter no. of candidates in waiting room : ");
-        waitingRoom=sc.nextInt();
-
-        for (int i=0;i<waitingRoom;i++)
+        candidatesWaiting=sc.nextInt();
+        for (int i=0;i<candidatesWaiting;i++)
         {
             queue.waitingRoom.add(queue.reception.remove());
         }
+
         do {
             System.out.println("Enter your choice");
             option=sc.nextInt();
@@ -38,32 +71,17 @@ public class QueueImpl {
             switch (option)
             {
                 case 1:
-                    System.out.println("List of candidates in reception"+queue.reception);
-                    System.out.println("List of candidates in waiting room"+queue.waitingRoom);
-                    System.out.println("In Interview Room -->  "+inRoom);
+                    queue.display();
                     break;
 
                 case 2://nextcandidate
-                    if(!isIn && !queue.waitingRoom.isEmpty())
-                    {
-                        isIn=!isIn;
-                        inRoom=queue.waitingRoom.remove();
-                        if(!queue.reception.isEmpty())
-                        queue.waitingRoom.add(queue.reception.remove());
-                        System.out.println(inRoom+" enters interview room");
-                    }
-                    else System.out.println("Candidate in Room");
+                    queue.callNextCandidate();
                     break;
 
-                case 3://send interviewee
-                    if(isIn)
-                    {
-                        System.out.println(inRoom+" leaves interview room");
-                        inRoom=null;
-                        isIn=!isIn;
+                case 3://send interviewee out
+                    if(queue.outCandidate())
                         ctr++;
-                    }
-                    else System.out.println("No one in interview room");
+                    break;
             }
 
         }while (ctr<candidates);
